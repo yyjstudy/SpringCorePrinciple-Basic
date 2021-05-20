fd8f0236f3de8bcab860262a65b4f29de5086b00
yongjo <yj92133581@gmail.com>
2021-05-20 14:16:09 +0900

    의존관계 자동 주입-조회 빈이 2개 이상-문제

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -13,7 +13,6 @@ import org.springframework.context.annotation.FilterType;
 )
 public class AutoAppConfig {
 
-    @Bean(name = "memoryMemberRepository")
     MemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
